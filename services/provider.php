e80fa78a5aa87b5ac9364891134cd5e2314c7ddb
Sergey Tolkachyov <musstudent@mail.ru>
2024-03-29 23:26:20 +0400

    update lang constants

--- services/provider.php
+++ services/provider.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * @package       WT JMoodle user sync
- * @version       1.0.0
+ * @version       1.0.1
  * @Author        Sergey Tolkachyov, https://web-tolk.ru
- * @сopyright (c) January 2024 Sergey Tolkachyov. All rights reserved.
+ * @сopyright (c) March 2024 Sergey Tolkachyov. All rights reserved.
  * @license       GNU/GPL3 http://www.gnu.org/licenses/gpl-3.0.html
  * @since         1.0.0
  */
